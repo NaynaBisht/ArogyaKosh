638e741f5c363692cdefbdc6fbfce4eb29759581
NaynaBisht1 <145500769+NaynaBisht@users.noreply.github.com>
2024-04-04 04:20:35 +0530

    Create patient.php

---
+++ patient.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patient</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+</head>
+<body>
+
+    <iframe src="header.php" style="height:65; width:100%;"></iframe>
+
+    <iframe src="footer.php" style="height:40%; width:100%;"></iframe>
+    
+</body>
+</html>
